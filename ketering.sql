7273e5bcb98d53fd83d22d0ad8ad4ae5600cdb3d
KingLordCompany <kinglordcompany@gmail.com>
2020-10-02 22:33:31 +0700

    dattabase sql

--- ketering.sql
+++ ketering.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Sep 2020 pada 13.18
+-- Waktu pembuatan: 02 Okt 2020 pada 17.32
 -- Versi server: 10.1.30-MariaDB
 -- Versi PHP: 7.2.2
 
@@ -39,6 +39,21 @@ CREATE TABLE `tb_admin` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tb_detail_produk`
+--
+
+CREATE TABLE `tb_detail_produk` (
+  `id_detail` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `nota_produk` int(11) NOT NULL,
+  `total harga` int(11) NOT NULL,
+  `catatan` varchar(30) NOT NULL,
+  `jumlah_pesan` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tb_pelanggan`
 --
 
@@ -46,11 +61,18 @@ CREATE TABLE `tb_pelanggan` (
   `id_pelanggan` int(11) NOT NULL,
   `nm_pelanggan` varchar(30) NOT NULL,
   `alamat` text NOT NULL,
-  `email` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `no_hp` varchar(15) NOT NULL,
   `password` varchar(128) NOT NULL,
   `tgl_daftar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_pelanggan`
+--
+
+INSERT INTO `tb_pelanggan` (`id_pelanggan`, `nm_pelanggan`, `alamat`, `email`, `no_hp`, `password`, `tgl_daftar`) VALUES
+(1, 'muhammad fahmy', 'melati', 'muhammadfahmy123@gmail.com', '085789456123', 'lahkokbisa', '2020-10-01 13:31:33');
 
 -- --------------------------------------------------------
 
@@ -59,7 +81,7 @@ CREATE TABLE `tb_pelanggan` (
 --
 
 CREATE TABLE `tb_pemesanan` (
-  `nota_pemesanan` varchar(20) NOT NULL,
+  `nota_pemesanan` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
   `tgl_order` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `tgl_antar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -87,7 +109,10 @@ CREATE TABLE `tb_produk` (
 --
 
 INSERT INTO `tb_produk` (`id_produk`, `nama_produk`, `harga`, `minimal_pesan`, `foto`, `deskripsi`) VALUES
-(1, 'Tempe', 200, 15, '70__51_1_0_20170117173726343_jpg_chip.jpg', 'asam manis');
+(2, 'Nasi Goreng', 12000, 15, 'download.jpg', 'Nasi Goreng Zimbabwe'),
+(3, 'Nasi Kuning', 13000, 15, 'nasi-kuning-rice-cooker.jpg', 'Nasi kuning Himalaya'),
+(4, 'Opor Ayam', 10000, 15, 'Resep-Opor-Ayam.jpg', 'Opor Ayam Bali'),
+(5, 'Sate', 10000, 15, 'Sate_Ponorogo.jpg', 'Sate Ponorogo');
 
 --
 -- Indexes for dumped tables
@@ -100,10 +125,25 @@ ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indeks untuk tabel `tb_detail_produk`
+--
+ALTER TABLE `tb_detail_produk`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD UNIQUE KEY `nota_produk` (`nota_produk`),
+  ADD UNIQUE KEY `id_produk` (`id_produk`);
+
+--
 -- Indeks untuk tabel `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  ADD PRIMARY KEY (`nota_pemesanan`),
+  ADD UNIQUE KEY `id_pelanggan` (`id_pelanggan`);
 
 --
 -- Indeks untuk tabel `tb_produk`
@@ -119,19 +159,48 @@ ALTER TABLE `tb_produk`
 -- AUTO_INCREMENT untuk tabel `tb_admin`
 --
 ALTER TABLE `tb_admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_detail_produk`
+--
+ALTER TABLE `tb_detail_produk`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  MODIFY `nota_pemesanan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_produk`
 --
 ALTER TABLE `tb_produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_detail_produk`
+--
+ALTER TABLE `tb_detail_produk`
+  ADD CONSTRAINT `tb_detail_produk_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `tb_produk` (`id_produk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_detail_produk_ibfk_2` FOREIGN KEY (`nota_produk`) REFERENCES `tb_pemesanan` (`nota_pemesanan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_pemesanan`
+--
+ALTER TABLE `tb_pemesanan`
+  ADD CONSTRAINT `tb_pemesanan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `tb_pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

84a5b99925c537609a446992edf93ca4fec9d072
KingLordCompany <kinglordcompany@gmail.com>
2020-12-18 23:31:32 +0700

    memperbaiki modal

--- application/views/admin/menunggu.php
+++ application/views/admin/menunggu.php
@@ -96,7 +96,7 @@
                                         <div class="modal-dialog modal-dialog-scrollable modal-xl">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">Edit <?= $judul ?></h5>
+                                                    <h5 class="modal-title" id="exampleModalLabel">Detail <?= $judul ?></h5>
                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                         <span aria-hidden="true">&times;</span>
                                                     </button>
